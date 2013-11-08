9d42398ce28d7ff9f40ba9238921d4d5e2ac05f0
michael ilori <ilori.mike@gmail.com>
2013-11-08 11:58:13 +0000

    upload test

---
+++ src/com/milo/parser/XMLVariableExtractor.java
@@ -0,0 +1,50 @@
+package com.milo.parser;
+
+import java.io.File;
+import java.util.Map;
+
+import org.dom4j.Document;
+
+
+public class XMLVariableExtractor implements VariableExtractor  {
+	private Document xmlDocument;
+	
+	public XMLVariableExtractor (File file)
+	{
+		
+	}
+	
+	public XMLVariableExtractor (String xmlString)
+	{
+		
+	}
+
+	@Override
+	public Map<String, String> validateVariables() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	protected Map<String, String> extractVariable()
+	{
+		return null;
+	}
+	
+	private Map<String, String> generateComposites()
+	{
+		return null;
+	}
+	
+
+	private void loadComposites()
+	{
+		
+	}
+	
+	private void loadAllRules ()
+	{
+		
+	}
+	
+	
+}
